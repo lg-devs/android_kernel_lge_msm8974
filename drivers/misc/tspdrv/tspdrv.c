543b6c25a4b79b3e6468b8685359a189adb7c5e8
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-05-04 01:42:32 +0100

    tspdrv: Add LG's timed_output based interface
    -Make the timed state atomic:
     This should fix race conditions with fast bursts of short tones
    -Add a sysfs control for timed vibrator strength

--- drivers/misc/tspdrv/tspdrv.c
+++ drivers/misc/tspdrv/tspdrv.c
@@ -55,6 +55,7 @@ static char IMMR_DEB = false;
 #endif
 
 #include "touch_fops.c"
+#include "imm_timed_output.h"
 /* Device name and version information */
 #define VERSION_STR " v3.7.11.0\n"                  /* DO NOT CHANGE - this is auto-generated */
 #define VERSION_STR_LEN 16                          /* account extra space for future extra digits in version number */
@@ -186,6 +187,35 @@ MODULE_AUTHOR("Immersion Corporation");
 MODULE_DESCRIPTION("TouchSense Kernel Module");
 MODULE_LICENSE("GPL v2");
 
+extern VibeInt8 timedForce;
+
+static ssize_t nforce_val_show(struct device *dev, struct device_attribute *attr,
+               char *buf)
+{
+	return sprintf(buf, "%hu", timedForce);
+}
+
+static ssize_t nforce_val_store(struct device *dev, struct device_attribute *attr,
+               const char *buf, size_t size)
+{
+	unsigned short int strength_val = DEFAULT_TIMED_STRENGTH;
+	if (kstrtoul(buf, 0, (unsigned long int*)&strength_val))
+               pr_err("[VIB] %s: error on storing nforce\n", __func__);
+
+
+       /* make sure new pwm duty is in range */
+	if (strength_val > 127)
+               strength_val = 127;
+	else if (strength_val < 1)
+               strength_val = 1;
+
+	timedForce = strength_val;
+
+	return size;
+}
+
+static DEVICE_ATTR(nforce_timed, S_IRUGO | S_IWUSR, nforce_val_show, nforce_val_store);
+
 #if 1
 /*                  
                                                     
@@ -304,7 +334,8 @@ static int __init tspdrv_init(void)
 
     }
 
-
+    device_create_file(&platdev.dev, &dev_attr_nforce_timed);
+    ImmVibe_timed_output();
     return 0;
 }
 
