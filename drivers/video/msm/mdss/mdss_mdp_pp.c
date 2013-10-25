950c00decdb81d0a23c6356211c82d8dc5e234ca
Krishna Chaitanya Parimi <cparimi@codeaurora.org>
2013-10-26 00:48:16 +0530

    mdss: Fix null point dereference in mdss_mdp_pp_init
    Fixed null point dereference in case of memory allocation
    failure in mdss_mdp_pp_init.
    
    Change-Id: I3593a583da355915b731ac30b1f38aaa010eba85
    Signed-off-by: Krishna Chaitanya Parimi<cparimi@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_pp.c
+++ drivers/video/msm/mdss/mdss_mdp_pp.c
@@ -2036,11 +2036,13 @@ int mdss_mdp_pp_init(struct device *dev)
 			mutex_unlock(&mdss_pp_mutex);
 			return ret;
 		#endif
-		}
-
-		for (i = 0; i < MDSS_MDP_MAX_DSPP; i++) {
-			mutex_init(&mdss_pp_res->dspp_hist[i].hist_mutex);
-			spin_lock_init(&mdss_pp_res->dspp_hist[i].hist_lock);
+		} else {
+			for (i = 0; i < MDSS_MDP_MAX_DSPP; i++) {
+				mutex_init(
+					&mdss_pp_res->dspp_hist[i].hist_mutex);
+				spin_lock_init(
+					&mdss_pp_res->dspp_hist[i].hist_lock);
+			}
 		}
 	}
 	if (mdata) {
