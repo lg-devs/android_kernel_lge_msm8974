fb4ce9b7a0f4eb505dca005256ec2e06201819c4
Adrian Salido-Moreno <adrianm@codeaurora.org>
2013-12-19 19:31:34 -0800

    msm: mdss: increase height constraints for panel resolution
    MDP is able to support higher resolution and is only limited in height
    by the MDP clock rate. There is already check in place for the MDP clock
    requirement, however need to update phase step calculation to handle
    proper overflow conditions for these higher resolution according to
    hardware constraints based on potential overflow while scaling with some
    phase steps. At the same time hardware is able to handle some conditions
    that are currently failed, so enable these to allow more cases that can
    go through MDP.
    
    Change-Id: I4c5a569e115302f05db7c525c0f8a76ed1738521
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -322,28 +322,31 @@ static int __mdss_mdp_overlay_setup_scaling(struct mdss_mdp_pipe *pipe)
 	int rc;
 
 	src = pipe->src.w >> pipe->horz_deci;
-	rc = mdss_mdp_calc_phase_step(src, pipe->dst.w, &pipe->phase_step_x);
-    if (rc == -EOVERFLOW) { 
-	/* overflow on horizontal direction is acceptable */ 
-		return 0; 
-	} else if (rc) { 
+	rc = mdss_mdp_calc_phase_step(src, pipe->dst.w,
+			&pipe->scale.phase_step_x[0]);
+	if (rc == -EOVERFLOW) {
+		/* overflow on horizontal direction is acceptable */
+		rc = 0;
+	} else if (rc) {
 		pr_err("Horizontal scaling calculation failed=%d! %d->%d\n",
 				rc, src, pipe->dst.w);
 		return rc;
 	}
 
 	src = pipe->src.h >> pipe->vert_deci;
-	rc = mdss_mdp_calc_phase_step(src, pipe->dst.h, &pipe->phase_step_y);
-	if ((rc == -EOVERFLOW) && (pipe->type == MDSS_MDP_PIPE_TYPE_VIG)) { 
-	/* overflow on Qseed2 scaler is acceptable */ 
-		return 0; 
-	} else if (rc) { 
+	rc = mdss_mdp_calc_phase_step(src, pipe->dst.h,
+			&pipe->scale.phase_step_y[0]);
+
+	if ((rc == -EOVERFLOW) && (pipe->type == MDSS_MDP_PIPE_TYPE_VIG)) {
+		/* overflow on Qseed2 scaler is acceptable */
+		rc = 0;
+	} else if (rc) {
 		pr_err("Vertical scaling calculation failed=%d! %d->%d\n",
 				rc, src, pipe->dst.h);
 		return rc;
 	}
 
-	return 0;
+	return rc;
 }
 
 static int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
