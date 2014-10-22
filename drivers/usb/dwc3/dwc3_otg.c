1afade3aeee59af0534d02522645c7e2684d45f9
Dorimanx <yuri@bynet.co.il>
2014-10-22 17:19:27 +0300

    FORCE FAST CHARGE: Delete useless FAKE AC hack. it's brake ROM LED.
    This has no effect on battery charge. it's just say AC and not USB
    But brake charge LED after unplug, it's still turned ON.
    and charge mode is not disabled, so wakelock is held, user loose
    power, as CPU dont sleep.
    
    Time for this HACK to be cleaned. it's was OFF by default.
    but users used it, and posted nags. purge is the best option here.
    
    Conflicts:
    	drivers/usb/dwc3/dwc3_otg.c

--- drivers/usb/dwc3/dwc3_otg.c
+++ drivers/usb/dwc3/dwc3_otg.c
@@ -709,17 +709,6 @@ static int dwc3_otg_set_power(struct usb_phy *phy, unsigned mA)
 #ifndef CONFIG_USB_DWC3_LGE_SINGLE_PSY
 	if (dwc3_otg_get_psy(phy) < 0)
 		goto psy_error;
-#ifdef CONFIG_FORCE_FAST_CHARGE
-		if ((force_fast_charge > 0) &&
-				(fake_charge_ac == FAKE_CHARGE_AC_ENABLE)) {
-			pr_info("msm_otg_notify_power_supply: "
-					"power_supply_get_by_name(ac)\n");
-			dotg->psy = power_supply_get_by_name("ac");
-		} else {
-			pr_info("msm_otg_notify_power_supply: "
-					"power_supply_get_by_name(usb)\n");
-			dotg->psy = power_supply_get_by_name("usb");
-		}
 #else
 	if (strcmp(dotg->psy->name, "usb")) {
 		pr_info("%s psy name is %s, so change psy to usb.\n", __func__, dotg->psy->name);
@@ -962,15 +951,7 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 	case OTG_STATE_UNDEFINED:
 		dwc3_otg_init_sm(dotg);
 		if (!dotg->psy) {
-#ifdef CONFIG_FORCE_FAST_CHARGE
-			if ((force_fast_charge > 0) &&
-					(fake_charge_ac == FAKE_CHARGE_AC_ENABLE))
-				dotg->psy = power_supply_get_by_name("ac");
-			else
-				dotg->psy = power_supply_get_by_name("usb");
-#else
 			dotg->psy = power_supply_get_by_name("usb");
-#endif
 
 			if (!dotg->psy)
 				dev_err(phy->dev,
