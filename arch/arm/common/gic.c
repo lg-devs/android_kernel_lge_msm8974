9240190dfae94538f1cea4da49c8e980582da89b
Youngjin Park <jin.park@lge.com>
2014-11-26 11:03:26 -0500

    LGE MOCA Driver
    Signed-off-by: Rashed Abdel-Tawab <rashed@linux.com>
    
    Conflicts:
    	arch/arm/common/gic.c

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -49,6 +49,7 @@
 
 #include <mach/socinfo.h>
 #include <mach/msm_rtb.h>
+#include <mach/moca_kernel_probe.h>
 
 union gic_base {
 	void __iomem *common_base;
@@ -259,6 +260,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 	     i = find_next_bit(pending, gic->max_irq, i+1)) {
 		pr_warning("%s: %d triggered", __func__,
 					i + gic->irq_offset);
+        IRQ_EVENT_PROBE(i + gic->irq_offset);
 	}
 }
 
