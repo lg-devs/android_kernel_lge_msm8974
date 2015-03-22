5e239bd3798766a3ffcc95623535ac1b71d1e33d
Rashed Abdel-Tawab <rashed@linux.com>
2015-03-21 23:54:47 -0400

    arm: Use board_panel_maker to correctly set panel variant

---
+++ arch/arm/mach-msm/include/mach/devices_lge.h
@@ -0,0 +1,26 @@
+/*
+ * LGE Panel Detection Header
+ *
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Author: Rashed Abdel-Tawab <rashed@linux.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __ARCH_ARM_MACH_MSM_LGE_DEVICES_LGE_H
+#define __ARCH_ARM_MACH_MSM_LGE_DEVICES_LGE_H
+
+/* Make board_panel_maker available for external use in order
+ * to correctly set the panel variant in setup.c
+ */
+extern int board_panel_maker;
+
+#endif /*__ARCH_ARM_MACH_MSM_LGE_DEVICES_LGE_H*/
